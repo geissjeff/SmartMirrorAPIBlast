a7fcb217dda77c5bd1bcc2e2cc773895929ea1ec
Justin <chan133@purdue.edu>
2019-02-10 16:41:43 -0500

    changed name to allow for nonimport error

--- weather.py
+++
@@ -1,7 +0,0 @@
-from weather import Weather, Unit
-
-weather = Weather(unit=Unit.CELSIUS)
-
-location = weather.lookup_by_location('indianapolis')
-condition = location.condition
-print(condition.text)
