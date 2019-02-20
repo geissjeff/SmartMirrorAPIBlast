a24ff5bb83490fc1c99fdcfc818b2a9f7f950785
Justin Chan <chan133@purdue.edu>
2019-02-20 11:13:39 -0500

    deleted calendar

--- calendar.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-base_url = "https://www.googleapis.com/calendar/v3/users/me/calendarList"
-response = requests.get(url = base_url, headers = {"Authorization": "Bearer ACCESS_TOKEN"},)
-response.raise_for_status()
-calendars = response.json().get("items")
