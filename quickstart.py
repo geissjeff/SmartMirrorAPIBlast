e287046a632f4470352edaa675f22d36a0937fd3
Justin <chan133@purdue.edu>
2019-03-24 14:55:57 -0400

    Refresh tokens first try

--- quickstart.py
+++ quickstart.py
@@ -2,12 +2,17 @@ from __future__ import print_function
 import datetime
 import pickle
 import os.path
+from oauth2client import GOOGLE_REVOKE_URI, GOOGLE_TOKEN_URI, client
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+CLIENT_ID = "671143266543-nhnrjkgvu64edkahkiv9edmit3l0uqhq.apps.googleusercontent.com" 
+CLIENT_SECRET = "_IhQ7c8vY4sLQo8CJXxL8KX0" 
+REFRESH_TOKEN = "1/HOYF-y6yl2FOSkxME3m0POyp0mmvEbKBc5H_1DMCGxU"
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -27,10 +32,23 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server()
+            #creds = flow.run_local_server()
+            creds = client.GoogleCredentials(
+                access_token = None,
+                client_id = CLIENT_ID, 
+                client_secret = CLIENT_SECRET, 
+                refresh_token = REFRESH_TOKEN,
+                token_expiry = None,
+                token_uri = GOOGLE_TOKEN_URI,
+                user_agent = None,
+                revoke_uri = GOOGLE_REVOKE_URI)
+            creds.refresh(httplib2.Http())
+            http = creds.authorize(httplib2.Http())
+            
+
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        #with open('token.pickle', 'wb') as token:
+        #    pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
 
@@ -49,3 +67,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+#Refresh token: 1/HOYF-y6yl2FOSkxME3m0POyp0mmvEbKBc5H_1DMCGxU
